fe84f5df3b8118e53936fe3fdeec6e30100644e0
Younus <alia.younus@accenture.com>
2020-07-27 17:15:58 -0500

    user db updated

--- user/src/errors/httpError.ts
+++
@@ -1,9 +0,0 @@
-// Base error to extend to other errors
-
-export abstract class HttpError extends Error {
-    statusCode:number
-    constructor(statusCode:number, message?:string){
-        super(message)
-        this.statusCode = statusCode
-    }
-}
