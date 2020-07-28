2772ece4c648736117de03ba83f57f148b76cc7b
Younus <alia.younus@accenture.com>
2020-07-27 22:46:06 -0500

    user dummy data added

--- user-database/user.sql
+++ user-database/user.sql
@@ -1,3 +1,4 @@
+
 create table users(
 "user_ID" int primary key,
 "Username" text unique not null,
@@ -6,3 +7,13 @@ create table users(
 "homeState" text not null,
 "picturePath" text
 );
+
+select * from users;
+
+insert into users("user_ID","Username","Password","Email","homeState","picturePath")
+     values ('1','Alia','password','alia@mail@mail.com', 'Texas',''),
+	        ('2','Emma','password','Emma@mail@mail.com', 'Newyork',''),
+			('3','Tanique','password','tanique@mail.com', 'Chicago',''),
+			('4','Cecilia','password','cecilia@mail.com', 'Ohio',''),
+			('5','Laura','password','laura@mail.com', 'Florida','');
+			 
