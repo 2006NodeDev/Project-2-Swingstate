8a05c60738094c4c1a114bddd5c59aa99a01827f
NeurogrammerT <bbstarnique@aol.com>
2020-07-28 11:10:25 -0400

    uploaded uniform sql scripts for user and state services

---
+++ state-service/SQL/swingstate_state_service_script.sql
@@ -0,0 +1,53 @@
+create schema swingstate_state_service;
+set schema 'swingstate_state_service';
+
+drop table polls;
+drop table states;
+
+create table states (
+	"state_id" serial primary key,
+	"stateName" text not null unique,
+	"democratic_candidate" text not null,
+	"republican_candidate" text not null,
+	"registration_link" text not null,
+	"voting_location" text,
+	"stateImage" text
+);
+
+insert into states ("stateName", "democratic_candidate", "republican_candidate", "registration_link", "voting_location", "stateImage")
+	values ('Alabama', 'Doug Jones', 'Tommy Tuberville', 'https://www.sos.alabama.gov/alabama-votes/voter/register-to-vote', '600 Dexter Avenue, Montgomery, AL 36104', null),
+			('Arizona', 'Mark Kelly', 'Martha McSally', 'https://azsos.gov/elections/voting-election/register-vote-or-update-your-current-voter-information', '1700 W Washington St, Phoenix, AZ 85007', null),
+			('Colorado', 'John Hickenlooper', 'Corey Gardner', 'https://www.vote.org/register-to-vote/colorado/?gclid=Cj0KCQjwyur0BRDcARIsAEt86ICGJciURTJTKsqa-rRzlH-3J9VagjKS48d0ti2Fc_hCShv3sdsQDhwaApTmEALw_wcB', '200 E Colfax Ave, Denver, CO 80203', null),
+			('Georgia', 'Jon Ossoff', 'David Perdue', 'https://sos.ga.gov/index.php/Elections/register_to_vote', '206 Washington St SW, Atlanta, GA 30334', null),
+			('Iowa', 'Theresa Greenfield', 'Joni Ernst', 'https://mymvd.iowadot.gov/Account/Login?ReturnUrl=%2fVoterRegistration', '1007 E Grand Ave, Des Moines, IA 50319', null),
+			('Kansas', 'Barbara Bollier', 'Lance Berland', 'http://www.voteks.org/before-you-vote/how-do-i-register.html.aspx', 'SW 8th & SW Van Buren, Topeka, KS 66612', null),
+			('Maine', 'Sarah Gideon', 'Susan Collins', 'https://www.maine.gov/sos/cec/elec/voter-info/votreg.html', 'Maine State House, Augusta, ME 04330', null),
+			('Michigan', 'Gary Peters', 'Jonathan James', 'https://mvic.sos.state.mi.us/RegisterVoter', '100 N Capitol Ave, Lansing, MI 48933', null),
+			('Montana', 'Steve Bullock', 'Steve Daines', 'https://sosmt.gov/elections/vote/', '1301 East Sixth Avenue, Helena, MT 59601', null),
+			('North Carolina', 'Cal Cunningham', 'Thom Tillis', 'https://www.ncsbe.gov/Voters/Registering-to-Vote', '1 E Edenton St, Raleigh, NC 27601', null);
+
+select * from states;
+
+create table polls(
+	poll_id serial primary key,
+	pollName text,
+	pollDate timestamp,
+	democratic_percent int,
+	republican_percent int,
+	state_id int references states ("state_id"),
+	margin int
+);
+
+insert into polls(pollName, pollDate, democratic_percent, republican_percent, state_id, margin)
+	values ('YouGov', now(), 51, 40, 1, 51-40),
+			('Public Policy Polling', now(), 47, 39, 2, 47-39),
+			('YouGov', now(), 46, 42, 3, 46-42),
+			('Public Policy Polling', now(), 46, 42, 4, 46-42),
+			('East Carolina University', now(), 41, 41, 5, 0),
+			('GQR Research', now(), 49, 47, 6, 2),
+			('Public Policy Polling', now(), 46, 44, 7, 2),
+			('Public Policy Polling', now(), 20, 21, 8, -1),
+			('Fox News', now(), 42, 45, 9, -3),
+			('Auburn University', now(), 36, 44, 10, -8);
+		
+select * from polls;
---
+++ user-service/SQL/swingstate_user_service_script.sql
@@ -0,0 +1,51 @@
+create schema swingstate_user_service;
+set schema 'swingstate_user_service';
+
+drop table user_state_bridge;
+drop table users;
+
+create table users(
+	"user_id" serial primary key,
+	"username" text unique not null,
+	"password" text not null,
+	"email" text not null,
+	"homeState" text,
+	"userImage" text
+);
+
+insert into users("user_id","username","password","email","homeState","userImage")
+     values ('1','Alia','password','alia@mail@mail.com', 'Texas','http://dummyimage.com/241x101.jpg/dddddd/000000'),
+	        ('2','Emma','password','emma@mail@mail.com', 'New York','http://dummyimage.com/241x101.jpg/dddddd/000000'),
+			('3','Tanique','password','tanique@mail.com', 'Chicago','http://dummyimage.com/154x146.png/5fa2dd/ffffff'),
+			('4','Cecilia','password','cecilia@mail.com', 'Ohio','http://dummyimage.com/154x146.png/5fa2dd/ffffff'),
+			('5','Laura','password','laura@mail.com', 'Florida','http://dummyimage.com/154x146.png/5fa2dd/ffffff'),
+			('6', 'zkenford5', 'iYkNkvw', 'ttremmel5@google.ca', 'New Jersey', 'http://dummyimage.com/130x176.bmp/dddddd/000000'),
+            ('7', 'aparradine6', 'wuc5DB', 'sgermann6@baidu.com', 'California', 'http://dummyimage.com/156x247.png/cc0000/ffffff'),
+            ('8', 'bkardos7', 'lQLYKuOqE', 'nsante7@ocn.ne.jp', 'Oregon', 'http://dummyimage.com/246x147.bmp/dddddd/000000'),
+            ('9', 'jjesson8', 'zIovBsrOYKJ', 'whaverson8@ameblo.jp', 'Virginia', 'http://dummyimage.com/112x105.png/dddddd/000000'),
+            ('10', 'pmolfino9', 'HWkWt4', 'gcolafate9@usgs.gov', 'Vermont', 'http://dummyimage.com/122x236.png/dddddd/000000');
+		
+select * from users;
+
+create table user_state_bridge(
+	"user_id" int references users ("user_id"),
+	"state_id" int references swingstate_state_service.states ("state_id"),
+	"updateFrequency" int not null,
+	"pollingThreshold" int not null
+);
+
+insert into user_state_bridge ("user_id", "state_id", "updateFrequency", "pollingThreshold")
+	values (1, 1, 7, 50),
+			(2, 2, 7, 25),
+			(3, 3, 7, 75),
+			(4, 4,14,50),
+			(5, 5, 14,25),
+			(1, 6, 14,75),
+			(2, 7, 21, 50),
+			(3, 8, 21, 25),
+			(4, 9, 21, 75),
+			(5, 10, 3, 10);
+
+select * from user_state_bridge;
+
+
