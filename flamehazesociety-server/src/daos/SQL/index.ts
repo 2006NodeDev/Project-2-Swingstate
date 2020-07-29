9e02a97d746f4505fce2c3c8d59f29cac0646865
benjamin skinner <benjamin.skinner15@gmail.com>
2020-07-29 15:05:36 -0400

    1 polling enpt completed

--- flamehazesociety-server/src/daos/SQL/index.ts
+++ flamehazesociety-server/src/daos/SQL/index.ts
@@ -1,8 +1,8 @@
 import { Pool } from 'pg'
 
 export const connectionPool:Pool = new Pool({
-    host: process.env['LB_HOST'],
-    user: process.env['LB_USER'],
+    host: process.env['LB_ADDRESS'],
+    user: process.env['LB_USERNAME'],
     password: process.env['LB_PASSWORD'],
     database: process.env['LB_DATABASE'],
     port:5432,
