12a2581fcf043a3422b3fa14fa2cbe7382650883
NeurogrammerT <bbstarnique@aol.com>
2020-07-30 15:21:12 -0400

    fixes

--- user-service/src/utils/AdditionalInfoDTO-to-AdditionalInfo.ts
+++ user-service/src/utils/AdditionalInfoDTO-to-AdditionalInfo.ts
@@ -1,10 +1,10 @@
-import {AdditionalUserInfo} from '../models/additonalUserInfo'
+import {AdditionalUserInfo} from '../models/AdditonalUserInfo'
 
 export function userInfoDTOToUserInfo(additionalUserInfoDTO){
     let additionalUserInfo:AdditionalUserInfo = {
         stateId:additionalUserInfoDTO.state_id,
-        updateFrequency:additionalUserInfoDTO.updateFrequency,
-        pollingThreshold:additionalUserInfoDTO.pollingThreshold
+        updateFrequency:additionalUserInfoDTO.update_frequency,
+        pollingThreshold:additionalUserInfoDTO.polling_threshold
     }
     return additionalUserInfo
 }
