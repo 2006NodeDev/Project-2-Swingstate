e3b0c5dc7a28069fb3c814d13f885c03d5e4318a
Younus <alia.younus@accenture.com>
2020-08-08 15:19:16 -0500

    updated with changes

--- RealTimeMessaging-Service/index.js
+++ RealTimeMessaging-Service/index.js
@@ -1,12 +1,16 @@
 var twilio = require('twilio');
 
 // sid and auth token in your Twilio account Console.
-var client = new twilio('AC749935d62082fdeef360c9138ad18c44', 'f85da8c79e793fe3f82228822d5a997d');
+var client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+//var client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+
+
 
 // Send the text message.
 client.messages.create({
-  to: '+19097148410',
-  from: '+15708730263',
+  to: '+1xxxx',
+  from: '+1xxxxxxx',
   body: 'Test Run - Election Results are up!'
 });
+
 
