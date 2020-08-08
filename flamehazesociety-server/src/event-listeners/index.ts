c9a2dafdb5d4fa03df4943bc79453b66ae821164
NeurogrammerT <bbstarnique@aol.com>
2020-08-08 18:14:57 -0400

    final version

--- flamehazesociety-server/src/event-listeners/index.ts
+++
@@ -1,8 +0,0 @@
-import {EventEmitter} from 'events'
-
-export const expressEventEmitter = new EventEmitter()
-
-export const customExpressEvents = {
-    NEW_USER: 'NEW_USER',
-    UPDATED_REIMBURSEMENT: 'UPDATED_REIMBURSEMENT'
-}
