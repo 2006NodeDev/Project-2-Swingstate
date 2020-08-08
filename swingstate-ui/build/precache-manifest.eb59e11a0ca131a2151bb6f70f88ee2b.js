84084e3f773674326d25c9597352817a15e8e00b
NeurogrammerT <bbstarnique@aol.com>
2020-08-08 15:47:08 -0400

    new ui build

---
+++ swingstate-ui/build/precache-manifest.eb59e11a0ca131a2151bb6f70f88ee2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fbbedf1f05177e25976d8730100824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fcae359161047481fe4",
+    "url": "/static/css/main.77b9c5c1.chunk.css"
+  },
+  {
+    "revision": "10c133758aca359579c1",
+    "url": "/static/js/2.ef7e1589.chunk.js"
+  },
+  {
+    "revision": "cf65305d934c66b1c53bc252e95eb343",
+    "url": "/static/js/2.ef7e1589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcae359161047481fe4",
+    "url": "/static/js/main.850e0ba3.chunk.js"
+  },
+  {
+    "revision": "7491ad572adf62a2fb1b",
+    "url": "/static/js/runtime-main.82322abc.js"
+  }
+]);
