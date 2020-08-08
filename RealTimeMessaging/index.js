1f6efb0664dba45396aedd98d4549380855f4ae8
aliayounus84 <43144586+aliayounus84@users.noreply.github.com>
2020-08-08 15:49:57 -0500

    Update index.js

--- RealTimeMessaging/index.js
+++ RealTimeMessaging/index.js
@@ -1,14 +1,13 @@
 var twilio = require('twilio');
 
 // sid and auth token in your Twilio account Console.
-// var client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN'); make these environmental variable
-var client = new twilio('AC749935d62082fdeef360c9138ad18c44', 'f85da8c79e793fe3f82228822d5a997d');
+var client = new twilio('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN'); //make these environmental variable
 
 
 // Send the text message.
 client.messages.create({
-  to: '+19097148410',
-  from: '+15708730263',
+  to: '+1phonenumber',
+  from: '+1phonenumber',
   body: 'Test Run - Election Results are up!'
 });
 
