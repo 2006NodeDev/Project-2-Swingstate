141ff419a39a3aeb3f1a9f2088a9d5da4e7499c6
Younus <alia.younus@accenture.com>
2020-08-08 15:08:14 -0500

    real time message service added

---
+++ RealTimeMessaging-Service/index.js
@@ -0,0 +1,12 @@
+var twilio = require('twilio');
+
+// sid and auth token in your Twilio account Console.
+var client = new twilio('AC749935d62082fdeef360c9138ad18c44', 'f85da8c79e793fe3f82228822d5a997d');
+
+// Send the text message.
+client.messages.create({
+  to: '+19097148410',
+  from: '+15708730263',
+  body: 'Test Run - Election Results are up!'
+});
+
