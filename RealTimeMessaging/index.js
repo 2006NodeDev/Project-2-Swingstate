b7f8f93d287083e074446266751f2bb45c6988b6
NeurogrammerT <bbstarnique@aol.com>
2020-08-09 11:46:52 -0400

    push

--- RealTimeMessaging/index.js
+++ RealTimeMessaging/index.js
@@ -1,12 +1,22 @@
+require('dotenv').config()
+
 var twilio = require('twilio');
 
+let username = process.env.TWILIO_ACCOUNT_SID
+
+let token = process.env.TWILIO_AUTH_TOKEN
+
+let toNumber = process.env.TO_PHONE_NUMBER
+
+let fromNumber = process.env.FROM_PHONE_NUMBER
+
 // sid and auth token in your Twilio account Console.
-var client = new twilio(process.env['TWILIO_ACCOUNT_SID'], process.env['TWILIO_AUTH_TOKEN']); //make these environmental variable
+var client = new twilio(username, token); //make these environmental variable
 
 
 // Send the text message.
 client.messages.create({
-  to: process.env['TO_PHONE_NUMBER'],
-  from: process.env['FROM_PHONE_NUMBER'],
+  to: toNumber,
+  from: fromNumber,
   body: 'Test Run - Election Results are up!'
 });
